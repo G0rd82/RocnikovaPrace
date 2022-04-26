fef2b1a38ca51fb2e6e2e6c39d43f4e9b5bcbdd4
pavel <pavelsipecek@gmail.com>
2022-04-26 18:37:32 +0200

    oznaceni_dne_pri_ulozeni

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const newEventModal = document.getElementById('newEventModal');
 const backDrop = document.getElementById('modalBackDrop');
 let clicked = null;
 
-openModal =(date)  =>{
+openModal = (date) => {
     clicked = date;
 
     const eventForDay = events.find(e => e.date === clicked);
@@ -21,7 +21,7 @@ openModal =(date)  =>{
     backDrop.style.display = 'block';
 }
 
-const closeModal = () =>{
+const closeModal = () => {
     eventTitleInput.classList.remove('error');
     newEventModal.style.display = 'none';
     deleteEventModal.style.display = 'none';
@@ -30,32 +30,32 @@ const closeModal = () =>{
     clicked = null;
 
 }
-isLeapYear = (year) =>{
-    return (year % 4===0 && year % 100 !== 0 && year % 400 !==0) || (year % 100 === 0 && year % 400 ===0)
+isLeapYear = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 === 0)
 }
 
-getFebDays = (year) =>{
+getFebDays = (year) => {
     return isLeapYear(year) ? 29 : 28
 }
 const month_names = ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec']
 
 let month_picker = document.querySelector("#month-picker")
 
-const generateCalendar = (month, year) =>{
+const generateCalendar = (month, year) => {
     let calendar_days = document.querySelector(".calendar-days")
-    calendar_days.innerHTML= ""
+    calendar_days.innerHTML = ""
     let calendar_header_year = document.querySelector("#year")
     let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    let currDate =new Date ()
+    let currDate = new Date()
 
     month_picker.innerHTML = month_names[month]
     calendar_header_year.innerHTML = year
     const first_day = new Date(year, month, 1);
 
-    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++){
+    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
         let day = document.createElement("div")
-        if(i >= first_day.getDay()){
+        if (i >= first_day.getDay()) {
             day.classList.add("calendar-day-hover")
             const eventForDay = events.find(e => e.date === first_day);
             day.innerHTML = i - first_day.getDay() + 1
@@ -63,9 +63,17 @@ const generateCalendar = (month, year) =>{
                               <span></span>
                               <span></span>
                               <span></span>`
-            if (i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()){
+            if (i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()) {
                 day.classList.add("curr-date")
             }
+
+            // Check Day
+            const eventForDayCheck = events.find(e => e.date === day.innerHTML);
+            if (eventForDayCheck !== undefined) {
+                day.classList.add('day-mark');
+            }
+            // ----------
+
             if (eventForDay) {
                 const eventDiv = document.createElement('div');
                 eventDiv.classList.add('event');
@@ -80,40 +88,56 @@ const generateCalendar = (month, year) =>{
 
 let currDate = new Date()
 let curr_month = {value: currDate.getMonth()}
-let curr_year= {value: currDate.getFullYear()}
+let curr_year = {value: currDate.getFullYear()}
 
 generateCalendar(curr_month.value, curr_year.value)
+
+// DayClicked Generator
+let dayClick;
+window.onclick = e => {
+    if (e.target.tagName === 'DIV') {
+        dayClick = e.target;
+    }
+}
+// --------------
 
 const saveEvent = () => {
     if (eventTitleInput.value) {
         eventTitleInput.classList.remove('error');
-
         events.push({
             date: clicked,
             title: eventTitleInput.value,
         });
+
+        // Update Day Icon
+        dayClick.classList.add('day-mark');
+
         closeModal();
         localStorage.setItem('events', JSON.stringify(events));
     } else {
         eventTitleInput.classList.add('error');
     }
 }
-const deleteEvent =() =>{
+const deleteEvent = () => {
     events = events.filter(e => e.date !== clicked);
+
+    // Update Day Icon
+    dayClick.classList.remove('day-mark');
+
     localStorage.setItem('events', JSON.stringify(events));
     closeModal();
 }
 
 document.querySelector("#prev-month").onclick = () => {
     --curr_month.value
-    if(curr_month.value<0)
-        curr_month.value=11;
+    if (curr_month.value < 0)
+        curr_month.value = 11;
     generateCalendar(curr_month.value, curr_year.value)
 };
 document.querySelector("#next-month").onclick = () => {
     ++curr_month.value
-    if(curr_month.value>=12)
-        curr_month.value=0;
+    if (curr_month.value >= 12)
+        curr_month.value = 0;
     generateCalendar(curr_month.value, curr_year.value)
 };
 document.querySelector("#prev-year").onclick = () => {
